fdf3f784a7c408d2fb636064a9b6ea1e85134e7c
Younes <younes@interactive-object.com>
2012-08-16 13:48:45 +0200

    add message for sensor

--- Younes/try2/app.js
+++ Younes/try2/app.js
@@ -3,157 +3,187 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , http = require('http');
+var express = require('express'), http = require('http');
 var events = require('events');
-var util = require('util'); 
+var util = require('util');
 var app = express();
 var _ = require('underscore');
 
 
 
-
-var data =
-{
-  rooms : ["Salle &agrave; manger", "Salon"],
-  devices : [ 
-            {id: 4, sensor: false, name : "Lampe central", room: 0, interruptor:true, color:"#000000"},
-            {id: 12, sensor: true, name : "Capteur IR", room: 0, startTime: 540, endTime:1020, duration:5 , color:"#000000", light:4},
-  				  {id: 21, sensor: false, name : "Lampe central", room: 1,  interruptor:true, color:"#000000"},
-            {id: 22, sensor: true, name : "Capteur IR", room: 1, startTime: 540, endTime:1020, duration: 10, color:"#000000", light:5 },
-            {id: 23, sensor: false, name : "entr&eacutee", room: 1, interruptor:true, color:"#000000"}
-            ]
-}
-
-
-            
-app.configure(function(){
-  app.set('port', process.env.PORT || 3003);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  //app.use(express.static(__dirname + '/public'));
-  app.use(express.static(require('path').resolve(__dirname + "/public")));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-app.get('/', function(req, res){
-  res.render('index', { title: 'App Domotique' });
-});
-
-app.get('/aboutus', function(req, res){
-  res.render('aboutus', { title: 'About us' });
-});
-
-app.get('/settings', function(req, res){
-    res.send(data);
-});
-
-app.post('/settings', function(req, res){
-    
-    
-    console.log(req.body.data)
-    data.devices = req.body.data;
-    
-    res.send(true);
-});
-
-app.post('/send', function(req, res){
-  var devise = req.body;
-  /*
-      send request via serial port here
-      
-      available properties for light :
-      - id, color(hex), interruptor(boolean)
-      
-      available properties for sensor :
-      - startTime(minute), endTime(minute), duration(seconde), color(hex), light
-      
-      var message =[];
-      message[0]= devise.id.toString(16);
-      message[1]= devise.color.toString(16);
-      serialPort.write(message);
-  */
-  var rgb = toRGB(devise.color);
-  var message = []
-  if(devise.sensor)
-  {
-    message[0] = parseInt(devise.id.toString(16);
-  }else {
-    message[0] = parseInt(devise.id.toString(16);
-  }
-  message[1] = rgb[0];
-  message[2] = rgb[1];  
-  message[3] = rgb[2];    
-  
-  res.send("ok");
-  
-});
-
-
-function toRGB(h) 
-{
-  var cut = (h.charAt(0)=="#") ? h.substring(1,7):h;
-  return [parseInt(cut.substring(0,2),16), parseInt(cut.substring(2,4),16), parseInt(cut.substring(4,6),16)];
-}
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
-
-
 var SerialPort = require("serialport").SerialPort;
-var serialPort = new SerialPort("/dev/ttyO1", {baudrate : 57600});
+var serialPort = new SerialPort("/dev/ttyO1", { baudrate : 57600 });
+
+var data = {
+	rooms : ["Salle à manger", "Salon", "Chambre"],
+	devices : [{
+			id : 4,
+			sensor : false,
+			name : "Lampe central",
+			room : 0,
+			interruptor : true,
+			color : "#000000"
+		}, {
+			id : 10,
+			sensor : true,
+			name : "Capteur IR",
+			room : 0,
+			startTime : 540,
+			endTime : 1020,
+			duration : 5,
+			color : "#000000",
+			light : 4
+		}, {
+			id : 21,
+			sensor : false,
+			name : "Lampe central",
+			room : 1,
+			interruptor : true,
+			color : "#000000"
+		}, {
+			id : 22,
+			sensor : true,
+			name : "Capteur IR",
+			room : 1,
+			startTime : 540,
+			endTime : 1020,
+			duration : 10,
+			color : "#000000",
+			light : 5
+		}, {
+			id : 23,
+			sensor : false,
+			name : "entr&eacutee",
+			room : 1,
+			interruptor : true,
+			color : "#000000"
+		}
+	]
+}
+
+app.configure(function () {
+	app.set('port', process.env.PORT || 3003);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	//app.use(express.static(__dirname + '/public'));
+	app.use(express.static(require('path').resolve(__dirname + "/public")));
+});
+
+app.configure('development', function () {
+	app.use(express.errorHandler());
+});
+
+app.get('/', function (req, res) {
+	res.render('index', {
+		title : 'App Domotique'
+	});
+});
+
+app.get('/aboutus', function (req, res) {
+	res.render('aboutus', {
+		title : 'About us'
+	});
+});
+
+app.get('/settings', function (req, res) {
+	res.send(data);
+});
+
+app.post('/settings', function (req, res) {
+	
+	console.log(req.body.data)
+	data.devices = req.body.data;
+	
+	res.send(true);
+});
+
+app.post('/send', function (req, res) {
+	var devise = req.body;
+	
+	// send message only for lights
+	if (devise.sensor) {
+		var rgb = toRGB(devise.color);
+		var message = []
+		message[0] = parseInt(devise.id.toString(16));
+		message[1] = rgb[0];
+		message[2] = rgb[1];
+		message[3] = rgb[2];
+		message[4] = devise.duration;
+		serialPort.write(message);
+		console.log(message);
+	}
+	res.send("ok");
+	
+});
+
 
 serialPort.on("data", function (data) {
-    var device_id= parseInt(data[0]); // device id sent from the sensor
-    console.log("device_id sent :"+device_id);
-    var device = _.find(data.devices, function(element){ return element.id == device_id; });
-    if(device)
-    {
-      if(device.sensor && device.running)
-      {
-        // send here the message to the light
-        var light_id = parseInt(device.light, 16);
-        
-      }
-    }else{
-      console.log("devise not found");
-    }
-    
+	console.log("here: " + data[3]);
+	var devise_id = parseInt(data[3]); // device id sent from the sensor
+	console.log("devise_id sent :" + devise_id);
+	var devise = _.find(data.devises, function (element) {
+			return element.id == device_id;
+		});
+	
+	if (devise) {
+		if (devise.sensor && devise.running) {
+			// send here the message to the light
+			
+			var rgb = toRGB(devise.color);
+			var message = []
+			message[0] = parseInt(device.light.toString(16));
+			message[1] = rgb[0];
+			message[2] = rgb[1];
+			message[3] = rgb[2];
+			message[4] = devise.duration;
+			serialPort.write(message);
+			console.log(message);
+			
+		}
+	} else {
+		console.log("devise not found");
+	}
+	
 });
 
+function toRGB(h) {
+	var cut = (h.charAt(0) == "#") ? h.substring(1, 7) : h;
+	return [parseInt(cut.substring(0, 2), 16), parseInt(cut.substring(2, 4), 16), parseInt(cut.substring(4, 6), 16)];
+}
 
-var interval = setInterval(function() {
-  var now = new Date;
-  var hour = now.getHours();
-  var minute = now.getMinutes();
-  var sminutes, shours, eminutes, ehours
-  
-  var sensors = _.filter(data.devices, function(d){ return d.sensor == true; });
-  _.each(sensors, function(sensor)
-  { 
-   	sminutes = parseInt(sensors.startTime % 60, 10),
-		shours = parseInt(sensors.startTime / 60 % 24, 10),
-    eminutes= parseInt(sensors.endTime % 60, 10),
-    ehours = parseInt(sensors.endTime / 60 % 24, 10);
-    
-    if (hour == shours && minute == sminutes && sensor.running == false) {
-      // TODO start listening the sensor here
-      sensor.running = true;
-    }else if(hour == ehours && minute == eminutes && sensor.running == true){
-      // TODO stop listening the sensor here
-      sensor.running = false;
-    }
-    
-    
-  });
+http.createServer(app).listen(app.get('port'), function () {
+	console.log("Express server listening on port " + app.get('port'));
+});
 
-}, 1000);
+var interval = setInterval(function () {
+		var now = new Date;
+		var hour = now.getHours();
+		var minute = now.getMinutes();
+		var sminutes,
+		shours,
+		eminutes,
+		ehours
+		
+		var sensors = _.filter(data.devices, function (d) {
+				return d.sensor == true;
+			});
+		_.each(sensors, function (sensor) {
+			sminutes = parseInt(sensors.startTime % 60, 10),
+			shours = parseInt(sensors.startTime / 60 % 24, 10),
+			eminutes = parseInt(sensors.endTime % 60, 10),
+			ehours = parseInt(sensors.endTime / 60 % 24, 10);
+			
+			if (hour == shours && minute == sminutes && sensor.running == false) {
+				sensor.running = true;
+			} else if (hour == ehours && minute == eminutes && sensor.running == true) {
+				sensor.running = false;
+			}
+			
+		});
+		
+	}, 1000);
