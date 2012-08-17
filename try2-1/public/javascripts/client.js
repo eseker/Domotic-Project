f107e08a8bb27d229b0eb446eb5fd420b243a86b
Younes <younes@interactive-object.com>
2012-08-17 21:18:53 +0300

    Update try2-1/public/javascripts/client.js

--- try2-1/public/javascripts/client.js
+++ try2-1/public/javascripts/client.js
@@ -172,7 +172,7 @@ $('li.color-thumb').live('click', function(){
       sensor_color.css("background-color", selected.color);  
     }else {
       	device_color.css("background-color", selected.color);
-	if(selected.color="#000000"){
+	if(selected.color=="#000000"){
     		interruptor_flip.val('off').slider("refresh");
     	}else {
     		interruptor_flip.val('on').slider("refresh");
